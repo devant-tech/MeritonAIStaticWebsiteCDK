368c175088468d237130d1f4b8c8f20823ae2058
Leo <liyung.chen@tum.de>
2025-06-17 22:37:26 +0200

    update redirect url

--- lib/constants/stages.ts
+++ lib/constants/stages.ts
@@ -17,7 +17,7 @@ export const STAGES = [
         env: { region: Region.US_EAST_1, account: AWS_ACCOUNT },
         staticAssetsBucketName: STATIC_ASSETS_BUCKET_DEV,
         googleClientId: "427948310600-v3g4op98fak6bo67r7q1to6duqnbvpnp.apps.googleusercontent.com",
-        googleRedirectUrl: `https://dev.${DOMAIN_NAME}`,
+        googleRedirectUrl: `https://dev.${DOMAIN_NAME}/auth/google/verify`,
         isProd: false
     },
     {
@@ -25,7 +25,7 @@ export const STAGES = [
         env: { region: Region.US_EAST_1, account: AWS_ACCOUNT }, // CLoudfront and ACM in us-east-1
         staticAssetsBucketName: STATIC_ASSETS_BUCKET_PROD,
         googleClientId: "427948310600-077gac1t6jc1p7dc0lo0ll8j4nrk4odo.apps.googleusercontent.com",
-        googleRedirectUrl: `https://${DOMAIN_NAME}`,
+        googleRedirectUrl: `https://${DOMAIN_NAME}/auth/google/verify`,
         isProd: true
     }
 ];
